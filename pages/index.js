4cb84a3b3414408094295dcea90eac599e858703
bjfoley21 <bjfoley21@gmail.com>
2023-05-26 23:52:39 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ import { BsBagCheckFill } from 'react-icons/bs';
 import { useStateContext } from '../context/StateContext';
 import { runFireworks } from '../lib/utils';
 
-print('jkjhhhrkjkjtfshhi');
+print('jkjhhhrkjkfsfdjtfshhi');
 const Schedule = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
   
